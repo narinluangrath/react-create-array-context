80ef04d90f7db8147f5a6d8b54e33a6d44e6d7a6
Narin Luangrath <narin.luangrath@gmail.com>
2021-09-20 19:00:43 -0700

    refactor: use context generator function

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,40 +1,87 @@
 // import * as React from 'react';
+// import { screen, render } from '@testing-library/react';
 
 import {
   calculateChangedBitsOfArray,
   // createArrayContext,
-  // useArrayContext,
 } from "./index";
+import type { ContextValue } from "./index";
 
-describe("calculateChangedBitsOfArray", () => {
-  let before: any[];
-  let after: any[];
+describe("helpers", () => {
+  describe("calculateChangedBitsOfArray", () => {
+    const before: ContextValue<string> = {
+      state: [],
+      setState: () => undefined,
+    };
+    const after: ContextValue<string> = {
+      state: [],
+      setState: () => undefined,
+    };
 
-  it("puts a 1 in the ith bit if the ith element changed", () => {
-    before = ["a", "b", "c"];
-    after = ["a", "g", "c"];
-    expect(calculateChangedBitsOfArray(before, after)).toBe(0b010);
+    it("puts a 1 in the ith bit if the ith element changed", () => {
+      before.state = ["a", "b", "c"];
+      after.state = ["a", "g", "c"];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b010);
 
-    before = ["a", "b", "c"];
-    after = ["a", "b", "g"];
-    expect(calculateChangedBitsOfArray(before, after)).toBe(0b100);
+      before.state = ["a", "b", "c"];
+      after.state = ["a", "b", "g"];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b100);
 
-    before = ["a", "b", "c"];
-    after = ["d", "e", "f"];
-    expect(calculateChangedBitsOfArray(before, after)).toBe(0b111);
-  });
+      before.state = ["a", "b", "c"];
+      after.state = ["d", "e", "f"];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b111);
+    });
 
-  it("handles empty arrays", () => {
-    before = [];
-    after = [];
-    expect(calculateChangedBitsOfArray(before, after)).toBe(0b0);
+    it("handles empty arrays", () => {
+      before.state = [];
+      after.state = [];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b0);
 
-    before = [];
-    after = ["a", "a", "a"];
-    expect(calculateChangedBitsOfArray(before, after)).toBe(0b111);
+      before.state = [];
+      after.state = ["a", "a", "a"];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b111);
 
-    before = ["a", "a", "a"];
-    after = [];
-    expect(calculateChangedBitsOfArray(before, after)).toBe(0b111);
+      before.state = ["a", "a", "a"];
+      after.state = [];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b111);
+    });
+
+    it("handle arrays of unequal size", () => {
+      before.state = ["a"];
+      after.state = ["b", "c", "d"];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b111);
+
+      before.state = ["a"];
+      after.state = ["a", "c", "d"];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b110);
+
+      before.state = ["a", "c", "d"];
+      after.state = ["a"];
+      expect(calculateChangedBitsOfArray(before, after)).toBe(0b110);
+    });
   });
 });
+
+// describe("react-create-array-context", () => {
+//   const ArrayContext = createArrayContext<string>([]);
+//   function Provider =
+
+//   it('handles no second argument to useArrayContext', () => {
+//     const value = ["a", "b", "c"];
+//     const mockOnRerender = jest.fn();
+//     const Consumer = ({ onRerender }: { onRerender: (x: any) => void }) => {
+//       const arrayContext = useArrayContext(ArrayContext);
+//       onRerender(arrayContext);
+//       return null;
+//     }
+
+//     render(
+//       <ArrayContext.Provider value={value}>
+//         <Consumer onRerender={mockOnRerender} />
+//       </ArrayContext.Provider>
+//     )
+
+//     expect(mockOnRerender).toHaveBeenCalledTimes(1);
+//     expect(mockOnRerender).toHaveBeenCalledWith(value);
+//   })
+// })
---
+++ src/index.tsx
@@ -0,0 +1,76 @@
+// eslint-disable-next-line
+import React, { createContext, useContext, useState } from "react";
+import type { Context, Dispatch, SetStateAction, ReactElement } from "react";
+
+export type ContextValue<T> = {
+  state: T[];
+  setState: Dispatch<SetStateAction<T[]>>;
+};
+
+export function calculateChangedBitsOfArray<T>(
+  contextValue: ContextValue<T>,
+  nextContextValue: ContextValue<T>
+) {
+  let changedBits = 0;
+
+  for (
+    let i = 0;
+    i < Math.max(contextValue?.state?.length, nextContextValue?.state?.length);
+    i++
+  ) {
+    const isChanged = contextValue?.state[i] !== nextContextValue?.state[i];
+    if (isChanged) {
+      const bit = 1 << i;
+      changedBits = changedBits | bit;
+    }
+  }
+
+  return changedBits;
+}
+
+type CreateArrayContext<T> = [
+  ({ initialState, ...rest }: { initialState: T[] }) => ReactElement,
+  (observedIndices?: number[]) => ContextValue<T>,
+  Context<ContextValue<T> | null>
+];
+
+export function createArrayContext<T>(): CreateArrayContext<T> {
+  const ArrayContext = createContext<ContextValue<T> | null>(
+    null,
+    // @ts-ignore (second argument of createContext is not documented)
+    calculateChangedBitsOfArray
+  );
+
+  function ArrayContextProvider({
+    initialState,
+    ...rest
+  }: {
+    initialState: T[];
+  }) {
+    const [state, setState] = useState<T[]>(initialState);
+    return <ArrayContext.Provider value={{ state, setState }} {...rest} />;
+  }
+
+  function useArrayContext(observedIndices: number[] = []) {
+    let observedBits = 0;
+
+    if (observedIndices.length) {
+      for (const index of observedIndices) {
+        observedBits = observedBits | index;
+      }
+    }
+
+    // @ts-ignore (second argument of useContext is not documented)
+    const contextValue = useContext(ArrayContext, observedBits || undefined);
+
+    if (!contextValue) {
+      throw Error(
+        "Must use `useArrayContext` inside of an `ArrayContextProvider`"
+      );
+    }
+
+    return contextValue;
+  }
+
+  return [ArrayContextProvider, useArrayContext, ArrayContext];
+}
